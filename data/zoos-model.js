5a802bd786749e01a07a4296fdec0b253a00a701
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-12 07:21:14 -0700

    added crud models

--- data/zoos-model.js
+++ data/zoos-model.js
@@ -6,6 +6,47 @@ const knexConfig = {
         filename:'./data/lambda.db3' //(b)
     },
     useNullAsDefault: true, //required only for sqlite3
-    debug: true,
+    debug: true
+}
 
-const db = knex(knexConfig)
+const db = knex(knexConfig);
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('zoos');
+}
+
+function findById(id) {
+    return db('zoos')
+    .where({ id })
+    .first()
+}
+
+async function add(zoo) {
+const [id] = await db('zoos').insert(zoo);
+
+return findById(id);
+}
+
+function update(id, changes) {
+    return db('zoos')
+    .where({ id })
+    .update(changes, '*')
+}
+
+function remove(id) {
+    return db('zoos')
+    .where({ id })
+    .del();
+}
+
+
+
+
